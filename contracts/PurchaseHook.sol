f8c872f26d627b9d1d56a392a7c1720b53eece1e
Julien Genestoux <julien.genestoux@gmail.com>
2022-04-03 21:38:35 -0400

    first commit

---
+++ contracts/PurchaseHook.sol
@@ -0,0 +1,102 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import "@unlock-protocol/contracts/dist/PublicLock/IPublicLockV9.sol";
+import "hardhat/console.sol";
+
+contract PurchaseHook {
+    address public secretSigner;
+    address public lock;
+
+    constructor(address _lock, address _secretSigner) {
+        secretSigner = _secretSigner;
+        lock = _lock;
+    }
+
+    function changeSigner(address _secretSigner) public {
+        require(
+            IPublicLockV9(lock).isLockManager(msg.sender),
+            "NOT_LOCK_MANAGER"
+        );
+        secretSigner = _secretSigner;
+    }
+
+    /**
+     * Debug function
+     */
+    function checkIsSigner(
+        string memory message,
+        bytes calldata signature /* data */
+    ) public view returns (bool isSigner) {
+        bytes memory encoded = abi.encodePacked(message);
+        bytes32 messageHash = keccak256(encoded);
+        bytes32 hash = ECDSA.toEthSignedMessageHash(messageHash);
+        address recoveredAddress = ECDSA.recover(hash, signature);
+        console.log(
+            "recoveredAddress %s : secretSigner %s",
+            recoveredAddress,
+            secretSigner
+        );
+        return recoveredAddress == secretSigner;
+    }
+
+    /**
+     * Price is the same for everyone... but we fail if signature does not match!
+     */
+    function keyPurchasePrice(
+        address from, /* from */
+        address, /* recipient */
+        address, /* referrer */
+        bytes calldata signature /* data */
+    ) external view returns (uint256 minKeyPrice) {
+        string memory message = toString(from);
+        require(checkIsSigner(message, signature), "WRONG_SIGNATURE");
+        if (address(msg.sender).code.length > 0) {
+            return IPublicLockV9(msg.sender).keyPrice();
+        }
+        return 0;
+    }
+
+    /**
+     * Helper functions to turn addrerss into string so we can verify the signature (address is signed as string on the client)
+     */
+    function toString(address account) public pure returns (string memory) {
+        return toString(abi.encodePacked(account));
+    }
+
+    function toString(uint256 value) public pure returns (string memory) {
+        return toString(abi.encodePacked(value));
+    }
+
+    function toString(bytes32 value) public pure returns (string memory) {
+        return toString(abi.encodePacked(value));
+    }
+
+    function toString(bytes memory data) public pure returns (string memory) {
+        bytes memory alphabet = "0123456789abcdef";
+
+        bytes memory str = new bytes(2 + data.length * 2);
+        str[0] = "0";
+        str[1] = "x";
+        for (uint256 i = 0; i < data.length; i++) {
+            str[2 + i * 2] = alphabet[uint256(uint8(data[i] >> 4))];
+            str[3 + i * 2] = alphabet[uint256(uint8(data[i] & 0x0f))];
+        }
+        return string(str);
+    }
+
+    /**
+     *
+     */
+    function onKeyPurchase(
+        address, /*from*/
+        address, /*recipient*/
+        address, /*referrer*/
+        bytes calldata, /*data*/
+        uint256, /*minKeyPrice*/
+        uint256 /*pricePaid*/
+    ) external {
+        /** no-op. this should have failed earlier if data is not the right signature  */
+    }
+}
---
+++ test/sample-test.js
@@ -0,0 +1,30 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("PurchaseHook", function () {
+  it("Should work", async function () {
+
+    const [user] = await ethers.getSigners();
+    const secretSigner = ethers.Wallet.createRandom()
+    const lock = "0xd0A031d9f9486B1D914124D0C1FCAC2e9e6504FE" // Not use here
+    const sender = "0xF5C28ce24Acf47849988f147d5C75787c0103534".toLowerCase()
+
+    const PurchaseHook = await ethers.getContractFactory("PurchaseHook");
+    const hook = await PurchaseHook.deploy(lock, secretSigner.address);
+    await hook.deployed();
+
+
+    // signing wrong message
+    expect(await hook.checkIsSigner(sender, await secretSigner.signMessage("hello"))).to.equal(false);
+    expect(await hook.checkIsSigner("hello", await secretSigner.signMessage(sender))).to.equal(false);
+
+    // wrong signer
+    expect(await hook.checkIsSigner(sender, await user.signMessage(sender))).to.equal(false);
+
+    // Correct signer, correct message
+    expect(await hook.checkIsSigner("hello", await secretSigner.signMessage("hello"))).to.equal(true);
+    expect(await hook.checkIsSigner(sender, await secretSigner.signMessage(sender))).to.equal(true);
+
+
+  });
+});
